74ebd082239e44b8981204920ac47de935d03eae
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-01 18:35:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+async function vald(e) {
+  e.preventDefault(); // stop page reload
+
+  let age = document.getElementById("age").value.trim();
+  let name = document.getElementById("name").value.trim();
+
+  // Validate
+  if (age === "" || name === "") {
+    alert("Please enter valid details");
+    return;
+  }
+
+  // Promise with delay
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
+
+  if (age < 18) {
+    alert("Oh sorry. You aren't old enough.");
+  } else {
+    alert("Welcome " + name + "!");
+  }
+}
